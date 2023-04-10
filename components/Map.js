0dbf7880c0b8cd66b482672995af416efaa438a2
xolekk <olek.stroinski@gmail.com>
2023-04-10 15:18:16 +0100

    Added Map and SelectLocation components

--- components/Map.js
+++ components/Map.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import mapboxgl from 'mapbox-gl'
+import { useEffect } from 'react'
+import 'mapbox-gl/dist/mapbox-gl.css'
+
+const style = {
+    wrapper: `flex-1 h-full w-full`,
+}
+
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
 
 const Map = () => {
-  
+    useEffect(()=>{
+        const map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/outdoors-v12',
+            center: [-0.15, 51.5],
+            zoom:10,
+        })
+    })
+
+
+    return <div className={style.wrapper} id='map' />
 }
 
 export default Map
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Navbar from "@/components/Navbar"
 import Map from "@/components/Map"
+import SelectLocation from "@/components/SelectLocation"
 
 const style ={
-  wrapper: `h-screen w-screen flex flex-col`
+  wrapper: `h-screen w-screen flex flex-col`,
+  main: `h-full w-screen flex-1 z-10`,
+  rideRequestContainer:`h-full w-[400px] ml-[1rem] py-[3rem] absolute top-0 left-0 flex flex-col justify-end z-20`,
+  rideRequest:`h-full max-h-[700px] bg-white rounded-lg flex flex-col overflow-scroll`,
 }
 
 export default function Home() {
@@ -10,14 +14,13 @@ export default function Home() {
    <div className={style.wrapper}>
     <Navbar />
     <div className={style.main}>
-      
+      <Map />
     </div>
-      <div className={style.requestArea}>
+    <div className={style.rideRequestContainer}>
         <div className={style.rideRequest}>
-
-
+        <SelectLocation/>
         </div>
       </div>
-   </div>
+    </div>
   )
 }
